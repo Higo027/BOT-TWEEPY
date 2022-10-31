b7890be2cab785312f8e87b219823ce78bc019e0
Higo Ian <101327686+Higo027@users.noreply.github.com>
2022-10-31 11:15:16 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,62 @@
+token = "AAAAAAAAAAAAAAAAAAAAAErCigEAAAAA1uRXir8CqVcX1bsOQZXmkOX9L44%3D0J9a9zHXwZ6BToAs6KPyBzdS6f4iz4JxF28IRiaDihjfiZVsc5" # bearer_token
+
+retweeted_today = 0
+
+words = [
+    '#RT_FLAMENGUISTA',
+    'flamengo',
+    'mengo',
+    'm3ngo',
+    'mengao',
+    'gavea'
+]
+
+words_not_accepted = [ #remove specific words and letters to filter with the best result, especially in Portuguese
+    'sexy',
+    'sex',
+    'porn',
+    'porno',
+    'pornô',
+    'naked',
+    'nude',
+    'nudes',
+    'game',
+    '&',
+    'к',
+    'ь',
+    'т',
+    'я',
+    'п'
+    'は',
+    'ダ',
+    'す',
+    '♪',
+    '本',
+    'の',
+    'べ',
+    'す',
+    '本',
+    'マ',
+    '在',
+    '应',
+    '用',
+    '糊',
+    '信',
+    'リ',
+    'ス',
+    'を',
+    'ー',
+    'न',
+    'ज',
+    'ä',
+    'ü',
+    'ö',
+    'ğ',
+    'ş',
+    'ر'
+]
+
+api_key = 'HAo05BxagWHxs3U5A0KZHWCo5'
+api_secret = 'WHg8qLFQbIkjVIlckT6Ze4vm5rzcM3a11v32BdWg9G4725NJwl'
+access_token = '1585932925190676481-V2t9M8WmBMfDsMQvNfHLAtdZ0eIfOZ'
+access_token_secret = 'NYvSh7BxVakbS6mNjCIKMSEJemoxsOgbWTQNWpAzy8mvP'
